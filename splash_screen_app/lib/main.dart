5ef036509a5a51b33a0473ede6f6efb778fe37b5
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-08-01 09:28:00 -0300

    Continuação
    Adição dos textos e cor de fundo na tela

--- splash_screen_app/lib/main.dart
+++ splash_screen_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -10,14 +9,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home:Splash(),
+      home: Splash(),
       debugShowCheckedModeBanner: false,
-
     );
   }
 }
+
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
@@ -27,17 +25,45 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   @override
-
-Widget build(BuildContext context) {
-   return Scaffold(
-      body: Center(
-        child: Container(
-          width:100,
-          height: 100,
-          child: RiveAnimation.asset('assets/gears.riv', ),
-        )
-      ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(color: Colors.indigo[800]),
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  height: 250,
+                  child: Text("Espere Só Mais Um Pouco",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      )),
+                  padding: EdgeInsets.only(
+                      left: 35.0, top: 125.0, right: 35.0, bottom: 35.0),
+                ),
+                Container(
+                  width: 150,
+                  height: 250,
+                  child: RiveAnimation.asset('assets/gears.riv'),
+                  padding: EdgeInsets.all(35.0),
+                ),
+                Container(
+                  height: 250,
+                  child: Text("Sua Aplicação Já Será Carregada...",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      )),
+                  padding: EdgeInsets.all(50.0),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
-
