629f90f7fa67f8b53561643f7beca38b07c11ebd
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-08-11 09:04:47 -0300

    Primeira faze concluida
    Finalização do design tela de login

--- login_pmovel_app/lib/main.dart
+++ login_pmovel_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,72 +12,118 @@ void main() {
           inputDecorationTheme: InputDecorationTheme(
               enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.white),
-                  borderRadius: BorderRadius.circular(10.0))))));
+                  borderRadius: BorderRadius.circular(5.0))))));
 }
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-  const _
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  bool checked = true;
+
+  void getCheckBoxValue(bool value) {
+    if (checked == false) {
+      setState(() {
+        checked = true;
+      });
+    } else {
+      setState(() {
+        checked = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(10), //Container(
-        /*decoration: BoxDecoration(
-          image: DecorationImage(
-              image: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa8qOI6zm91XI9-vwOkyHI_zDqivONwGlWUQ&usqp=CAU").image
-          ),
-        ),*/
+        padding: EdgeInsets.fromLTRB(10.0, 70.0, 10.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-                padding: EdgeInsets.all(10.0),
+                height: 200.0,
+                //padding: EdgeInsets.all(10.0),
                 child: Image.network(
                     "https://www.pmovel.com.br/images/logo_v.png")),
-            Divider(),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "E-mail ou CPF",
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white54, width: 2.0),
-                    borderRadius: BorderRadius.circular(10.0)),
-                prefixIcon: Padding(
-                  padding: EdgeInsetsDirectional.only(start: 12.0),
-                  child: Icon(Icons.person, color: Colors.white),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "E-mail ou CPF",
+                  labelStyle: TextStyle(color: Colors.white),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(5.0)),
+                  prefixIcon: Padding(
+                    padding: EdgeInsetsDirectional.only(start: 12.0),
+                    child: Icon(Icons.person, color: Colors.white),
+                  ),
                 ),
+                style: TextStyle(color: Colors.white, fontSize: 15),
               ),
-              style: TextStyle(color: Colors.white, fontSize: 15),
             ),
-            Divider(),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white54, width: 2.0),
-                    borderRadius: BorderRadius.circular(10.0)),
-                prefixIcon: Padding(
-                  padding: EdgeInsetsDirectional.only(start: 12.0),
-                  child: Icon(Icons.lock, color: Colors.white),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.white),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(5.0)),
+                  prefixIcon: Padding(
+                    padding: EdgeInsetsDirectional.only(start: 12.0),
+                    child: Icon(Icons.lock, color: Colors.white),
+                  ),
                 ),
+                style: TextStyle(color: Colors.white, fontSize: 15),
               ),
-              style: TextStyle(color: Colors.white, fontSize: 15),
             ),
             Row(
               children: [
-                CheckboxListTile(value: false, onChanged: bool)
+                Checkbox(
+                  value: checked,
+                  onChanged: (value) {
+                    getCheckBoxValue(value!);
+                  },
+                  activeColor: Colors.white,
+                  checkColor: Colors.red,
+                ),
+                Text(
+                  "Lembrar senha?",
+                  style: TextStyle(color: Colors.white),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 100.0),
+                  child: Text(
+                    "Esqueceu a Senha?",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ],
-            )
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Acesssar",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.lightBlue),
+                ),
+              ),
+            ),
           ],
         ),
       ),
