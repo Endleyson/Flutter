15cb358679a892be4950ad0bc190002aeb4602b7
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-09-03 13:49:35 -0300

    Continuação

--- crypto_app/lib/screens/coins_screen.dart
+++ crypto_app/lib/screens/coins_screen.dart
@@ -1,35 +1,57 @@
+import 'package:crypto_app/models/coins.dart';
 import 'package:crypto_app/providers/coins.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CryptoApp extends StatefulWidget {
-  CryptoApp({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  CryptoApp({Key? key}) : super(key: key);
 
   @override
   _CryptoAppState createState() => _CryptoAppState();
 }
 
+
 class _CryptoAppState extends State<CryptoApp> {
   @override
   Widget build(BuildContext context) {
     final table = CoinsProvider.table;
+    NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+    List<Coins> selections = [];
     return Scaffold(
         appBar: AppBar(
-          title: Text("CryptoValues"),
+          title: Text("Crypto Values",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
           centerTitle: true,
-          elevation: 0,
+          elevation: 3,
         ),
         body: ListView.separated(
-          itemBuilder: (BuildContext context, int coins){
+          itemBuilder: (BuildContext context, int coins) {
             return ListTile(
-              leading: Image.asset(table[coins].icon, height:40, width:40),
-              title: Text(table[coins].name),
-              trailing: Text(table[coins].price.toString()),
-            );
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+                leading:(selections.contains(table[coins]))
+                    ?CircleAvatar(
+                    child: Icon(Icons.check)
+                ) :
+                SizedBox(child: Image.asset(table[coins].icon, width: 40)),
+                title: Text(
+                  table[coins].name,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                trailing: Text(
+                  real.format(table[coins].price),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                selected: selections.contains(table[coins]),
+                selectedTileColor: Colors.deepOrange[50],
+                onLongPress: () {
+                  setState(() {(selections.contains(table[coins]))?selections.remove(table[coins]) : selections.add(table[coins]);
+                  }
+                  );
+                });
           },
-          padding: EdgeInsets.all(16.0),
-          separatorBuilder: (_, __)=>Divider(),
+          padding: EdgeInsets.all(20.0),
+          separatorBuilder: (_, __) => Divider(),
           itemCount: table.length,
         ));
   }
